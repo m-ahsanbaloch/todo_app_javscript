3f1b43bbc9ceb4ff479204d7167f5ed9943fda0a
ahsansamad <ingeniouslogo@gmail.com>
2024-06-01 03:54:14 +0500

    completed Added Some Storage Feature

--- app.js
+++ app.js
@@ -14,31 +14,55 @@ function addTask(event) {
   // ================= if value entered ==========
   else {
     var li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+    var date = new Date();
+    var hourss = date.getHours().toString();
+    if (hourss <= 12) {
+      var ampm = "Am";
+    } else {
+      var ampm = "Pm";
+    }
+    date = date.toString();
+    console.log(hourss);
+    var myDate = ` &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<b style ="color:#555;font-size:14px;"> ${date.slice(
+      0,
+      24
+    )}:${ampm}</b>`;
+    li.innerHTML = inputBox.value + myDate;
     listContainer.appendChild(li);
     var span = document.createElement("span");
     span.innerHTML = "X";
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
   // ================= if value entered ==========
 }
 // ========== Add Task Function =========
 
 // ======================== Delete Task Function ===============
-listContainer.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName == "LI") {
-      e.target.classList.toggle("checked");
-    } else if (e.target.tagName == "SPAN") {
-      swal("Are you sure?", {
-        dangerMode: true,
-        buttons: true,
-      });
-      e.target.parentElement.remove();
-    }
-  },
-  false
-);
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName == "SPAN") {
+    swal("Are you sure?", {
+      dangerMode: true,
+      buttons: true,
+    });
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
 // ======================== Delete Task Function ===============
+
+// ====================== Local Storage Saving Date =================
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+// ====================== Local Storage Saving Date =================
+
+// =================== local Storage Getting Date ==================
+function getData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+getData();
+// =================== local Storage Getting Date ==================
